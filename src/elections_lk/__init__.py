07fc4828a6f4b5d87122343456e83f4844b01d03
nuuuwan <nuuuwan@gmail.com>
2024-09-23 14:39:49 +0530

    lint

--- src/elections_lk/__init__.py
+++ src/elections_lk/__init__.py
@@ -1,23 +1,5 @@
 # elections_lk (auto generate by build_inits.py)
 
-from elections_lk.core import (
-    Party,
-    Votes,
-)
-
-from elections_lk.core import (
-    Election,
-    ElectionBase,
-    ElectionGIGData,
-)
-
-from elections_lk.core import (
-    ElectionCategory,
-    ElectionPresidential,
-)
-
-from elections_lk.core import (
-    PartyToVotes,
-    Result,
-    VoteSummary,
-)
+from elections_lk.core import (Election, ElectionBase, ElectionCategory,
+                               ElectionGIGData, ElectionPresidential, Party,
+                               PartyToVotes, Result, Votes, VoteSummary)
