05c7f87f69a7807588f2c957db512ba17ee3575e
nuuuwan <nuuuwan@gmail.com>
2024-09-23 14:24:23 +0530

    deleted analysis

--- src/elections_lk/__init__.py
+++ src/elections_lk/__init__.py
@@ -1,6 +1,5 @@
 # elections_lk (auto generate by build_inits.py)
 
-from elections_lk.analysis import ProjectionModel, ProjectionSeries, Similarity
 from elections_lk.core import (Election, ElectionBase, ElectionCategory,
                                ElectionGIGData, ElectionPresidential, Party,
                                PartyToVotes, Result, Votes, VoteSummary)
--- src/elections_lk/analysis/__init__.py
+++
@@ -1,5 +0,0 @@
-# elections_lk.analysis (auto generate by build_inits.py)
-
-from elections_lk.analysis.ProjectionModel import ProjectionModel
-from elections_lk.analysis.ProjectionSeries import ProjectionSeries
-from elections_lk.analysis.Similarity import Similarity
