766796f13c6a6540327d5cf53602bdd78e77f830
nuuuwan <nuuuwan@gmail.com>
2024-10-15 11:41:33 +0530

    VERSION = "3.0.2"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 DIST_NAME = 'elections_lk'
-VERSION = "3.0.1"
+VERSION = "3.0.2"
 DESCRIPTION = "Python Library for Sri Lankan Elections Data."
 INSTALL_REQUIRES = [
     'utils-nuuuwan',
