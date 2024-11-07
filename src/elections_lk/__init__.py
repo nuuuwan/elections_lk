ed4163c5d2fee81117e176fffa1dde4cc648bd75
nuuuwan <nuuuwan@gmail.com>
2024-11-07 18:19:57 +0530

    Deleted analysis

--- src/elections_lk/__init__.py
+++ src/elections_lk/__init__.py
@@ -1,6 +1,5 @@
 # elections_lk (auto generate by build_inits.py)
 
-from elections_lk.analysis import Correlation, SeatAllocation
 from elections_lk.constants import YEAR_TO_REGION_TO_SEATS
 from elections_lk.core import (Election, ElectionBase, ElectionCategory,
                                ElectionGIGData, ElectionParliamentary,
--- src/elections_lk/analysis/__init__.py
+++
@@ -1,4 +0,0 @@
-# elections_lk.analysis (auto generate by build_inits.py)
-
-from elections_lk.analysis.Correlation import Correlation
-from elections_lk.analysis.SeatAllocation import SeatAllocation
