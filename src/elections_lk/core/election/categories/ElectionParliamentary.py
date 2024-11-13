daeb736f4e9de2f8a6f5e6efe3e7cc92d2196b9b
nuuuwan <nuuuwan@gmail.com>
2024-11-13 20:05:54 +0530

    Updated region_to_party_to_seats & lint

--- src/elections_lk/core/election/categories/ElectionParliamentary.py
+++ src/elections_lk/core/election/categories/ElectionParliamentary.py
@@ -2,8 +2,9 @@ from functools import cached_property
 
 from elections_lk.constants import YEAR_TO_REGION_TO_SEATS
 from elections_lk.core.election.base.Election import Election
-from elections_lk.core.election.categories.ElectionCategory import \
-    ElectionCategory
+from elections_lk.core.election.categories.ElectionCategory import (
+    ElectionCategory,
+)
 from elections_lk.core.Seats import Seats
 
 
@@ -14,18 +15,18 @@ class ElectionParliamentary(Election):
     @staticmethod
     def get_years() -> list[str]:
         return [
-            '1989',
-            '1994',
-            '2000',
-            '2001',
-            '2004',
-            '2010',
-            '2015',
-            '2020',
+            "1989",
+            "1994",
+            "2000",
+            "2001",
+            "2004",
+            "2010",
+            "2015",
+            "2020",
         ]
 
     @staticmethod
-    def list_all() -> list['ElectionParliamentary']:
+    def list_all() -> list["ElectionParliamentary"]:
         return [
             ElectionParliamentary(year)
             for year in ElectionParliamentary.get_years()
@@ -37,14 +38,19 @@ class ElectionParliamentary(Election):
 
     @cached_property
     def region_to_party_to_seats(self) -> dict[str, dict[str, int]]:
-        return {
-            region_id: Seats.get_party_to_seats(
-                region_id, n_seats, self.get_result(region_id).party_to_votes
+        idx = {}
+        for region_id, n_seats in self.region_to_seats.items():
+            result = self.get_result(region_id)
+            idx[region_id] = (
+                Seats.get_party_to_seats(
+                    region_id, n_seats, result.party_to_votes
+                )
+                if result
+                else 0
             )
-            for region_id, n_seats in self.region_to_seats.items()
-        }
+        return idx
 
     @cached_property
     def cum_party_to_seats(self) -> dict[str, int]:
         unsorted = Seats.concat(*self.region_to_party_to_seats.values())
-        return Seats.sort(unsorted, self.get_result('LK').party_to_votes)
+        return Seats.sort(unsorted, self.get_result("LK").party_to_votes)
