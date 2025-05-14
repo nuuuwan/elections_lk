3447ac764bb063ad6d575d3b48eef9f860903797
nuuuwan <nuuuwan@gmail.com>
2025-05-14 11:35:22 +0530

    [examples/presidential_elections.py] added formatting

--- examples/presidential_elections.py
+++ examples/presidential_elections.py
@@ -2,14 +2,16 @@ from elections_lk import ElectionPresidential
 
 
 def main():
+    print("-" * 30)
+    print("Year".ljust(5), "Turnout".rjust(10), "Rejected".rjust(10))
     for election in ElectionPresidential.list_all():
+        vote_summary = election.lk_result.vote_summary
         print(
-            election.year,
-            election.lk_result.vote_summary.polled,
-            election.lk_result.vote_summary.p_turnout,
-            election.lk_result.vote_summary.p_valid,
-            election.lk_result.vote_summary.p_rejected,
+            election.year.ljust(5),
+            f"{vote_summary.p_turnout:.2%}".rjust(10),
+            f"{vote_summary.p_rejected:.2%}".rjust(10),
         )
+    print("-" * 30)
 
 
 if __name__ == "__main__":
