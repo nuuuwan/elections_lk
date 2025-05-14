3eb1cd22599fe8f61e609b2c3b72e1d04a545d38
nuuuwan <nuuuwan@gmail.com>
2025-05-14 16:39:10 +0530

    test_vote_summary

---
+++ tests/test_vote_summary.py
@@ -0,0 +1,20 @@
+import unittest
+from elections_lk import VoteSummary
+
+
+class TestCase(unittest.TestCase):
+    def test_vote_summary(self):
+        vote_summary = VoteSummary(
+            electors=100,
+            polled=70,
+            valid=63,
+            rejected=7,
+        )
+        self.assertEqual(vote_summary.electors, 100)
+        self.assertEqual(vote_summary.polled, 70)
+        self.assertEqual(vote_summary.valid, 63)
+        self.assertEqual(vote_summary.rejected, 7)
+
+        self.assertAlmostEqual(vote_summary.p_turnout, 0.7)
+        self.assertAlmostEqual(vote_summary.p_valid, 0.9)
+        self.assertAlmostEqual(vote_summary.p_rejected, 0.1)
