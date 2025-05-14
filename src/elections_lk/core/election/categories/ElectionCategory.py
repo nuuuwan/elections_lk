d6831f7994a7e09f3f3b401d609edd2f614aa07c
nuuuwan <nuuuwan@gmail.com>
2025-05-14 16:35:31 +0530

    [src/elections_lk/core/election/categories/ElectionCategory.py] Comment out - Not implemented as yet

--- src/elections_lk/core/election/categories/ElectionCategory.py
+++ src/elections_lk/core/election/categories/ElectionCategory.py
@@ -2,6 +2,8 @@ class ElectionCategory:
     PRESIDENTIAL = "presidential"
     PARLIAMENTARY = "parliamentary"
     PARLIAMENTARY_FPTP = "parliamentary-fptp"
-    PROVINCIAL_COUNCIL = "provincial-council"
-    LOCAL_GOVERNMENT = "local-government"
-    REFERENDUM = "referendum"
+
+    # Not implemented as yet
+    # PROVINCIAL_COUNCIL = "provincial-council"
+    # LOCAL_GOVERNMENT = "local-government"
+    # REFERENDUM = "referendum"
