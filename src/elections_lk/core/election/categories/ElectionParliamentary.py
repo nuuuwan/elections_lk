7d351126fcba327b0c547d676885efa6eb4a862b
nuuuwan <nuuuwan@gmail.com>
2025-05-14 13:33:44 +0530

    Move examples to unittests

--- src/elections_lk/core/election/categories/ElectionParliamentary.py
+++ src/elections_lk/core/election/categories/ElectionParliamentary.py
@@ -4,9 +4,8 @@ from gig import EntType
 
 from elections_lk.constants import YEAR_TO_REGION_TO_SEATS
 from elections_lk.core.election.base.Election import Election
-from elections_lk.core.election.categories.ElectionCategory import (
-    ElectionCategory,
-)
+from elections_lk.core.election.categories.ElectionCategory import \
+    ElectionCategory
 from elections_lk.core.Seats import Seats
 
 
---
+++ tests/test_election.py
@@ -0,0 +1,14 @@
+import unittest
+
+from elections_lk import (ElectionLocalGovernment, ElectionParliamentary,
+                          ElectionPresidential)
+
+
+class TestCase(unittest.TestCase):
+    def test_elections(self):
+        for election in [
+            ElectionPresidential("2024"),
+            ElectionParliamentary("2024"),
+            ElectionLocalGovernment("2025"),
+        ]:
+            print(election.lk_result)
