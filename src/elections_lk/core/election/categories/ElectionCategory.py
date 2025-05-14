122c57f788d78449c3865a5db611aa057f8bafd4
nuuuwan <nuuuwan@gmail.com>
2025-05-14 09:02:17 +0530

    Added more ElectionCategory

--- src/elections_lk/core/election/categories/ElectionCategory.py
+++ src/elections_lk/core/election/categories/ElectionCategory.py
@@ -1,3 +1,7 @@
 class ElectionCategory:
-    PRESIDENTIAL = 'presidential'
-    PARLIAMENTARY = 'parliamentary'
+    PRESIDENTIAL = "presidential"
+    PARLIAMENTARY = "parliamentary"
+    PARLIAMENTARY_FPTP = "parliamentary_fptp"
+    PROVINCIAL_COUNCIL = "provincial_council"
+    LOCAL_GOVERNMENT = "local_government"
+    REFERENDUM = "referendum"
