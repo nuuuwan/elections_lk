b1513ea955ffbfebc09bd33a5b3cb60500f49f57
nuuuwan <nuuuwan@gmail.com>
2025-05-14 13:54:33 +0530

    Removed comments

--- tests/test_election.py
+++ tests/test_election.py
@@ -1,13 +1,17 @@
 import unittest
 
-from elections_lk import ElectionLocalGovernment
+from elections_lk import (
+    ElectionLocalGovernment,
+    ElectionParliamentary,
+    ElectionPresidential,
+)
 
 
 class TestCase(unittest.TestCase):
     def test_elections(self):
         for election in [
-            # ElectionPresidential("2024"),
-            # ElectionParliamentary("2024"),
+            ElectionPresidential("2024"),
+            ElectionParliamentary("2024"),
             ElectionLocalGovernment("2025"),
         ]:
             print(election.lk_result)
