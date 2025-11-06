a31c06ef43efd0909b03bff18ecb3326c8a2ba6f
nuuuwan <nuuuwan@gmail.com>
2025-11-06 09:07:50 +0530

    Enhance visualization of rejected votes by calculating and displaying mean and 95% confidence intervals

--- examples/rejected_votes/__main__.py
+++ examples/rejected_votes/__main__.py
@@ -1,25 +1,52 @@
-import matplotlib.pyplot as plt
 import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+from utils import Log
+
 from elections_lk import ElectionParliamentary
+
+log = Log(os.path.basename(os.path.dirname(__file__)))
 
 
 if __name__ == "__main__":
     years = []
-    rejected_rates = []
+    p_rejected = []
     for election in ElectionParliamentary.list_all():
         if election.year == "2000":
             continue
         years.append(int(election.year))
         vote_summary = election.lk_result.vote_summary
-        rejected_rates.append(vote_summary.p_rejected)
+        p_rejected.append(vote_summary.p_rejected)
 
-    plt.bar(years, rejected_rates, color='r')
+    mean_p_rejected = np.mean(p_rejected)
+    std_p_rejected = np.std(p_rejected)
+    dist = stats.norm(loc=mean_p_rejected, scale=std_p_rejected)
+    ci_lower, ci_upper = dist.interval(0.95)
+
+    plt.figure(figsize=(16, 9))
+    plt.bar(years, p_rejected, color="r")
+    plt.axhline(y=mean_p_rejected, color="grey", linestyle="--", label="Mean")
+    plt.axhline(
+        y=ci_lower,
+        color="blue",
+        linestyle=":",
+        label="95% CI Lower",
+    )
+    plt.axhline(
+        y=ci_upper,
+        color="red",
+        linestyle=":",
+        label="95% CI Upper",
+    )
     plt.title("Rejected Votes in Sri Lankan Parliamentary Elections")
     plt.xlabel("Election Year")
     plt.ylabel("Rejected Votes (%)")
+    plt.legend()
     plt.gca().yaxis.set_major_formatter(
         plt.FuncFormatter(lambda y, _: f"{y:.1%}")
     )
-    plt.savefig(os.path.join(os.path.dirname(__file__), "rejected_votes.png"))
-
-    
+    image_path = os.path.join(os.path.dirname(__file__), "rejected_votes.png")
+    plt.savefig(image_path)
+    log.info(f"Wrote {image_path}")
